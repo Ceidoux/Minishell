35ad14d75749715fcce9db5f728207da4be617c3
jle <114746132+jle-git@users.noreply.github.com>
2023-06-20 23:29:14 +0200

    working: table_of_commands (unstable)

--- srcs/parser/parse.c
+++ srcs/parser/parse.c
@@ -1,7 +1,10 @@
 #include "minishell.h"
 
-static char	***ft_create_toc(t_list	*tokens);
-static char	***ft_add_command_line(char ***toc, t_list *tokens);
+static char	***ft_create_table_of_commands(t_list *tokens);
+static void ft_add_command(char ***toc, char *command);
+static void ft_add_input(char ***toc, char *input);
+static void ft_add_output(char ***toc, char *output);
+static char	***ft_add_new_line(char ***toc);
 static int	ft_toc_size(char ***toc);
 static void	ft_toc_cpy(char ***dst_toc, char ***src_toc);
 static void	ft_toc_free(char ***toc);
@@ -12,64 +15,76 @@ char	***ft_parser(char *s, char **envp)
 	t_list	*tokens;
 
 	tokens = ft_lexer(s, envp);
-	toc = ft_create_toc(tokens);
 	ft_lstprint(tokens);
+	toc = ft_create_table_of_commands(tokens);
 	ft_lstclear(&tokens, &free);
 	return (toc);
 }
 
-static char	***ft_create_toc(t_list	*tokens)
+static char	***ft_create_table_of_commands(t_list	*tokens)
 {
 	char	***toc;
 	
 	toc = ft_calloc(1, sizeof(*toc));
 	if (!toc)
-		exit(FAILURE);
+		exit(EXIT_FAILURE);
 	*toc = ft_calloc(3, sizeof(**toc));
 	if (!(*toc))
-		exit(FAILURE);	
+		exit(EXIT_FAILURE);	
 	while (tokens)
 	{
-		if (tokens->type == OPERATOR && !ft_strncmp(tokens->type, "<", 2))
-			ft_add_input(toc, tokens);
-		else if (tokens->type == OPERATOR && !ft_strncmp(tokens->type, ">", 2))
-			ft_add_output(toc, tokens);
+		if (tokens->type == OPERATOR && !ft_strncmp(tokens->content, "<", 2))
+		{
+			tokens = tokens->next;
+			ft_add_input(toc, tokens->content);
+		}
+		else if (tokens->type == OPERATOR && !ft_strncmp(tokens->content, ">", 2))
+		{
+			tokens = tokens->next;
+			ft_add_output(toc, tokens->content);
+		}
 		else if (tokens->type == WORD)
-			ft_add_command(toc, tokens);
+			ft_add_command(toc, tokens->content);
 		else
-			toc = ft_add_command_line(toc, tokens);
+			toc = ft_add_new_line(toc);
 		tokens = tokens->next;
 	}
 	return (toc);
 }
 
-static void ft_add_command(char ***toc, t_list tokens)
+static void ft_add_command(char ***toc, char *command)
+{
+	int		last_line;
+	char	*temp;
+
+	last_line = ft_toc_size(toc) - 1;
+	temp = toc[last_line][0];
+	toc[last_line][0] = ft_strjoin(toc[last_line][0], " ");
+	free(temp);
+	temp = toc[last_line][0];
+	toc[last_line][0] = ft_strjoin(toc[last_line][0], command);
+	free(temp);
+}
+
+static void ft_add_input(char ***toc, char *input)
 {
 	int	last_line;
 
 	last_line = ft_toc_size(toc) - 1;
-	toc[last_line][1] = ft_strdup(tokens->content);
+	toc[last_line][1] = ft_strdup(input);
 }
 
-static void ft_add_input(char ***toc, t_list tokens)
+static void ft_add_output(char ***toc, char *output)
 {
 	int	last_line;
 
 	last_line = ft_toc_size(toc) - 1;
-	toc[last_line][1] = ft_strdup(tokens->content);
+	toc[last_line][2] = ft_strdup(output);	
 }
 
-static void ft_add_output(char ***toc, t_list tokens)
+static char	***ft_add_new_line(char ***toc)
 {
-	int	last_line;
-
-	last_line = ft_toc_size(toc) - 1;
-	toc[last_line][2] = ft_strdup(tokens->content);	
-}
-
-static char	***ft_add_command_line(char ***toc, t_list *tokens)
-{
-	char **new_toc;
+	char ***new_toc;
 	int	toc_size ;
 	
 	toc_size = ft_toc_size(toc);
@@ -80,7 +95,7 @@ static char	***ft_add_command_line(char ***toc, t_list *tokens)
 	ft_toc_free(toc);
 	new_toc[toc_size] = ft_calloc(3, sizeof(**new_toc));
 	if (!(new_toc[toc_size]))
-		exit(FAILURE);	
+		exit(EXIT_FAILURE);	
 	return (new_toc);
 }
 
@@ -89,11 +104,11 @@ static void	ft_toc_free(char ***toc)
 	int	toc_size;
 	int	toc_column;
 
-	toc_size = ft_toc_size(src_toc);
+	toc_size = ft_toc_size(toc);
 	while (--toc_size)
 	{
 		toc_column = -1;
-		while (++toc_colum < 3)
+		while (++toc_column < 3)
 			free(toc[toc_size][toc_column]);
 	}
 }
@@ -107,10 +122,10 @@ static void	ft_toc_cpy(char ***dst_toc, char ***src_toc)
 	{
 		dst_toc[toc_size] = ft_calloc(3, sizeof(**dst_toc));
 		if (!(dst_toc[toc_size]))
-			exit(FAILURE);	
-		ft_strlcpy(dst_toc[toc_size][0], src_toc[toc_size][0]);
-		ft_strlcpy(dst_toc[toc_size][1], src_toc[toc_size][1]);
-		ft_strlcpy(dst_toc[toc_size][2], src_toc[toc_size][2]);
+			exit(EXIT_FAILURE);	
+		ft_strlcpy(dst_toc[toc_size][0], src_toc[toc_size][0], ft_strlen(src_toc[toc_size][0]) + 1);
+		ft_strlcpy(dst_toc[toc_size][1], src_toc[toc_size][1], ft_strlen(src_toc[toc_size][0]) + 1);
+		ft_strlcpy(dst_toc[toc_size][2], src_toc[toc_size][2], ft_strlen(src_toc[toc_size][0]) + 1);
 	}
 }
 
