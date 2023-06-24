502389231cf1d0f7fb8863dfa9e58096d86e0916
jle <114746132+jle-git@users.noreply.github.com>
2023-06-24 19:27:38 +0200

    fixed: echo $ser$USER

--- srcs/lexer/expanding.c
+++ srcs/lexer/expanding.c
@@ -1,6 +1,6 @@
 #include "minishell.h"
 
-static char	*ft_expand_var(char *s, int idx);
+static int	ft_expand_var(char **s, int idx);
 
 /* expansion des variables unquoted
 nb: char** car on passe l'adresse d'un str */
@@ -17,7 +17,7 @@ void	ft_expand(char **s)
 	{
 		if ((*s)[idx] == '$' && simple_quote == FALSE)
 		{
-			*s = ft_expand_var(*s, idx + 1);
+			idx += ft_expand_var(s, idx + 1);
 			continue;
 		}
 		else if ((*s)[idx] == '\"' && simple_quote == FALSE)
@@ -28,30 +28,33 @@ void	ft_expand(char **s)
 	}
 }
 
-static char	*ft_expand_var(char *s, int idx)
+static int	ft_expand_var(char **s, int idx)
 {
 	char	*var_name;
 	char	*var;
 	char	*new_s;
+	char	*old_s;
 	int		len;
 
 	len = 0;
-	while (ft_isalnum(s[len + idx]) || s[len + idx] == '_')
+	while (ft_isalnum((*s)[len + idx]) || (*s)[len + idx] == '_')
 		len++;
 	if (!len)
-		return (s);
-	var_name = ft_substr(s, idx, len);
+		return (1);
+	var_name = ft_substr(*s, idx, len);
 	var = getenv(var_name);
 	free(var_name);
-	new_s = malloc((ft_strlen(s) - len + ft_strlen(var)) * sizeof(*new_s));
+	new_s = malloc((ft_strlen(*s) - len + ft_strlen(var)) * sizeof(*new_s));
 	if (!new_s)
-		return (NULL);
-	ft_strlcpy(new_s, s, idx);
+		exit(EXIT_FAILURE);
+	ft_strlcpy(new_s, *s, idx);
 	if (var)
 		ft_strlcpy(new_s + idx - 1, var, ft_strlen(var) + 1);
-	ft_strlcpy(new_s + idx - 1 + ft_strlen(var), s + idx + len, ft_strlen(s) - (idx + len) + 1);
-	free(s);
-	return (new_s);
+	ft_strlcpy(new_s + idx - 1 + ft_strlen(var), *s + idx + len, ft_strlen(*s) - (idx + len) + 1);
+	old_s = *s;
+	*s = new_s;
+	free(old_s);
+	return (ft_strlen(var));
 }
 
 /*
