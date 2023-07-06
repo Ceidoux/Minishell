61d7cd6f73bbda56939769ab54b773154a840087
Seydou Mestre-Kone <smestrekone@protonmail.com>
2023-07-06 17:21:28 +0200

    fixed : pwd exit status

--- srcs/builtins/pwd.c
+++ srcs/builtins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 14:39:40 by smestre           #+#    #+#             */
-/*   Updated: 2023/07/06 16:17:23 by ubuntu           ###   ########.fr       */
+/*   Updated: 2023/07/06 17:09:40 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,16 +50,16 @@ int	ft_pwd(t_tools tools, t_cmd_tab toc, char **envp)
 {
 	char	*current_dir;
 
-	if (tools.args[1])
-	{
-		if (invalid_option(tools.args[1]))
-			return (g_exit_status = 2, 0);
-		else
-			g_exit_status = 0;
-	}
 	tools.pid[tools.i] = fork();
 	if (tools.pid[tools.i] == 0)
 	{
+		if (tools.args[1])
+		{
+			if (invalid_option(tools.args[1]))
+				exit(2);
+			else
+				exit(0);
+		}
 		ft_pipe_manager(tools, toc);
 		current_dir = NULL;
 		current_dir = getcwd(current_dir, 4096);
