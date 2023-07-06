cbc399aacde56b2320ea4aa5e74cbf7d85cd43f0
Seydou Mestre-Kone <smestrekone@protonmail.com>
2023-07-06 15:49:59 +0200

    fixed: norm change in echo

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,13 +6,29 @@
 /*   By: ubuntu <ubuntu@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/05 14:16:25 by smestre           #+#    #+#             */
-/*   Updated: 2023/07/06 15:36:00 by ubuntu           ###   ########.fr       */
+/*   Updated: 2023/07/06 15:48:42 by ubuntu           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-
+int	not_quote(char*	str, int *i, char c)
+{
+	(*i)++;
+	if (!str[*i] || str[*i] != '-')
+		return (0);
+	(*i)++;
+	if ((!str[*i]) || (str[*i] && str[*i] != 'n'))
+		return (0);
+	while (str[*i] == 'n')
+		(*i)++;
+	if (!str[*i] || str[*i] != c)
+		return (0);
+	(*i)++;
+	if (str[*i] && str[*i] != ' ')
+		return (0);
+	return (1);
+}
 int	is_slash_n(char *str)
 {
 	int i;
@@ -20,35 +36,13 @@ int	is_slash_n(char *str)
 	i = 0;
 	if (str[i] == '\"')
 	{
-		i++;
-		if (!str[i] || str[i] != '-')
-			return (0);
-		i++;
-		if ((!str[i]) || (str[i] && str[i] != 'n'))
-			return (0);
-		while (str[i] == 'n')
-			i++;
-		if (!str[i] || str[i] != '\"')
-			return (0);
-		i++;
-		if (str[i] && str[i] != ' ')
+		if (!not_quote(str, &i, '\"'))
 			return (0);
 		return (i);
 	}
 	if (str[i] == '\'')
 	{
-		i++;
-		if (!str[i] || str[i] != '-')
-			return (0);
-		i++;
-		if ((!str[i]) || (str[i] && str[i] != 'n'))
-			return (0);
-		while (str[i] == 'n')
-			i++;
-		if (!str[i] || str[i] != '\'')
-			return (0);
-		i++;
-		if (str[i] && str[i] != ' ')
+		if (!not_quote(str, &i, '\''))
 			return (0);
 		return (i);
 	}
