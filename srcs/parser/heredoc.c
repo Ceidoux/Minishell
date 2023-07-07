7ad9c6ec48f823bd4f7e0f2c987452631e8bfd4c
jle <114746132+jle-git@users.noreply.github.com>
2023-07-07 14:02:05 +0200

    norm: heredoc.c

--- srcs/parser/heredoc.c
+++ srcs/parser/heredoc.c
@@ -12,14 +12,22 @@
 
 #include "minishell.h"
 
-static int	ft_create_heredoc(char *delimiter, char **envp);
+static int	ft_readline_heredoc(char *delimiter, char **envp, int fd);
 
 int	ft_heredoc(char *delimiter, char **envp)
 {
 	int	fd;
+	int	tp_exit_status;
 
-	if (ft_create_heredoc(delimiter, envp) == -1)
+	fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0666);
+	if (fd == -1)
 		return (-1);
+	tp_exit_status = g_exit_status;
+	g_exit_status = -1;
+	if (ft_readline_heredoc(delimiter, envp, fd) == -1)
+		return (close(fd), -1);
+	g_exit_status = tp_exit_status;
+	close(fd);
 	fd = open(".heredoc", O_RDONLY);
 	if (fd == -1)
 		return (-1);
@@ -27,24 +35,18 @@ int	ft_heredoc(char *delimiter, char **envp)
 	return (fd);
 }
 
-static int	ft_create_heredoc(char *delimiter, char **envp)
+static int	ft_readline_heredoc(char *delimiter, char **envp, int fd)
 {
-	int		fd;
 	char	*line;
 	int		idx;
-	int		tp_exit_status;
 
-	fd = open(".heredoc", O_WRONLY | O_CREAT | O_TRUNC, 0666);
-	if (fd == -1)
-		return (-1);
 	idx = 0;
-	tp_exit_status = g_exit_status;
-	g_exit_status = -1;
+	line = NULL;
 	while (++idx)
 	{
 		line = readline(">");
 		if (g_exit_status == 130)
-			return (free(line), close(fd), -1);
+			return (free(line), -1);
 		else if (!line || !ft_strncmp(line, delimiter, ft_strlen(delimiter) + 1))
 			break ;
 		if (idx > 1)
@@ -54,11 +56,10 @@ static int	ft_create_heredoc(char *delimiter, char **envp)
 		free(line);
 	}
 	write(fd, "\n", 1);
-	g_exit_status = tp_exit_status;
 	free(line);
-	close(fd);
 	return (0);
 }
+
 /*
 export VAR=END
 cat << $VAR		-> le délimitateur est $VAR
