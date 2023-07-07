598be9f9ede1d787681c1a350a2ab9430e2139a9
jle <114746132+jle-git@users.noreply.github.com>
2023-07-07 13:47:47 +0200

    norm unquote.c

--- srcs/converter/unquote.c
+++ srcs/converter/unquote.c
@@ -37,7 +37,8 @@ static int	ft_remove_quotes(char **s, int idx, char c)
 		exit(EXIT_FAILURE);
 	ft_strlcpy(new_s, *s, idx + 1);
 	ft_strlcpy(new_s + idx, *s + idx + 1, len + 1);
-	ft_strlcpy(new_s + idx + len, *s + idx + len + 2, ft_strlen(*s) - idx - len - 1);
+	ft_strlcpy(new_s + idx + len, *s + idx + len + 2,
+		ft_strlen(*s) - idx - len - 1);
 	free(*s);
 	*s = new_s;
 	return (len - 1);
